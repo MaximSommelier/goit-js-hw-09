d9388c88c5147ebdcb7c24a8cfee6947038f3816
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-17 22:47:53 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const body = document.querySelector("body");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+startBtn.addEventListener('click', onStartClick);
+stopBtn.addEventListener('click', onStopClick);
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  };
+
+function onStartClick(evt){
+    timerId = setInterval(()=>{
+    body.style.background = getRandomHexColor();
+},1000);
+};
+
+function onStopClick(evt){
+   clearInterval(timerId)
+};
+// console.log(colorBody);
+
+
+
 
