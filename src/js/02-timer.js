89f159ece72793458bb4be1728ed0f700d5b2d79
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-18 16:41:59 +0300

    css

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,6 +8,18 @@ const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
 
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+
+const fp = flatpickr(dateTime, {});
+
 // dateTime.addEventListener('input', inputTime);
 // startBtn.addEventListener('click', startCountdown);
 
