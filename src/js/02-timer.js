d2c3eac61d7c1d37412c751111f4e04d627cc573
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-18 14:49:04 +0300

    Update 02-timer.js

---
+++ src/js/02-timer.js
@@ -0,0 +1,10 @@
+const dateTime = document.querySelector("#datetime-picker");
+const startBtn = document.querySelector('[data-start]');
+const days = document.querySelector('[data-days]');
+const hours = document.querySelector('[data-hours]');
+const minutes = document.querySelector('[data-minutes]');
+const seconds = document.querySelector('[data-seconds]');
+
+// dateTime.addEventListener('input', inputTime);
+// startBtn.addEventListener('click', startCuntdown);
+
