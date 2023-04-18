ddebc927335d2886bb023d09053c0e27a564c1d1
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-19 02:37:51 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -9,7 +9,9 @@ const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
 
-// startBtn.addEventListener('click', startCountdown);
+startBtn.addEventListener('click', () =>{
+  reverseTimer.start();
+});
 
 const options = {
     enableTime: true,
@@ -20,30 +22,62 @@ const options = {
       console.log(selectedDates[0]);
     },
   };
-   
-
+  
 const fp = flatpickr(dateTime, options);
+const startTime = options.selectedDates[0];
 
+const reverseTimer = {
+  inrevalId: null,
+  isActive: false,
+
+  start (){
+    if (this.isActive){
+      return;
+    }
+    this.isActive = true;
+    this.inrevalId = setInterval (() =>{
+     const currentTime = Date.now();
+     const ms = startTime - currentTime;
+     const { days, hours, minutes, seconds } = addLeadingZero(convertMs(ms));
+
+      },1000);
+    },
+    stop (){
+      if(currentTime === startTime){
+        clearInterval(this.inrevalId);
+      this.isActive = false;
+      }
+    },
+};
+reverseTimer.stop();
+
+function updateReverseTimer ({ days, hours, minutes, seconds }) {
+  days.textContent = `${days}`;
+  hours.textContent = `${hours}`;
+  minutes.textContent = `${minutes}`;
+  seconds.textContent = `${seconds}`;
+};
+
+function addLeadingZero(value) {
+return String(value).padStart(2, '0');
+};
 
 function convertMs(ms) {
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-        const days = Math.floor(ms / day);
-        const hours = Math.floor((ms % day) / hour);
-        const minutes = Math.floor(((ms % day) % hour) / minute);
-        const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      
-        return { days, hours, minutes, seconds };
-      };
-    
-    
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
+  return { days, hours, minutes, seconds };
+};
 
 // startBtn.disabled = true;
 // Notiflix.Notify.failure('Please choose a date in the future');
 
-// dateTime.addEventListener('input', inputTime);
+
 
 
