5c29abb4db9e548115667c6640a611440f7e000d
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-18 23:33:01 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
 
 const dateTime = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector('[data-start]');
@@ -7,6 +8,22 @@ const days = document.querySelector('[data-days]');
 const hours = document.querySelector('[data-hours]');
 const minutes = document.querySelector('[data-minutes]');
 const seconds = document.querySelector('[data-seconds]');
+
+// startBtn.addEventListener('click', startCountdown);
+
+function convertMs(ms) {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+  
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  
+    return { days, hours, minutes, seconds };
+  }
 
 const options = {
     enableTime: true,
@@ -18,8 +35,13 @@ const options = {
     },
   };
 
-const fp = flatpickr(dateTime, {});
+const fp = flatpickr(dateTime, options);
+
+
+
+// startBtn.disabled = true;
+// Notiflix.Notify.failure('Please choose a date in the future');
 
 // dateTime.addEventListener('input', inputTime);
-// startBtn.addEventListener('click', startCountdown);
+
 
