4e2ddfd13b208e4cd5ef3b0f8378dac1125abe1e
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-19 17:36:29 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -4,44 +4,44 @@ import Notiflix from 'notiflix';
 
 const dateTime = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector('button[data-start]');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
+// const days = document.querySelector('[data-days]');
+// const hours = document.querySelector('[data-hours]');
+// const minutes = document.querySelector('[data-minutes]');
+// const seconds = document.querySelector('[data-seconds]');
 
-startBtn.addEventListener('click', () =>{
-  timer.start();
-});
+//   class ReverseTimer  {
+//     constructor ({onTick}){
+//      this.inrevalId = null;
+//      this.isActive = false; 
+//      this.onTick = onTick;
+//     }
 
-  class ReverseTimer  {
-    constructor ({onTick}){
-     this.inrevalId = null;
-     this.isActive = false; 
-     this.onTick = onTick;
-    }
+//     start (){
+//     if (this.isActive){
+//       return;
+//     }
+//     this.isActive = true;
+//     this.inrevalId = setInterval (() =>{
+//      const currentTime = Date.now();
+//      const ms = startTime - currentTime;
+//      const { days, hours, minutes, seconds } = addLeadingZero(convertMs(ms));
+//      updateReverseTimer(ms);
+//       },1000);
+//     }
 
-    start (){
-    if (this.isActive){
-      return;
-    }
-    this.isActive = true;
-    this.inrevalId = setInterval (() =>{
-     const currentTime = Date.now();
-     const ms = startTime - currentTime;
-     const { days, hours, minutes, seconds } = addLeadingZero(convertMs(ms));
-     updateReverseTimer(ms);
-      },1000);
-    }
+//     stop (){
+//       if(currentTime === startTime){
+//         clearInterval(this.inrevalId);
+//       this.isActive = false;
+//       updateReverseTimer(0);
+//       }
+//       return;
+//     }
+//   };
 
-    stop (){
-      if(currentTime === startTime){
-        clearInterval(this.inrevalId);
-      this.isActive = false;
-      updateReverseTimer(0);
-      }
-      return;
-    }
-  };
+//   startBtn.addEventListener('click', () =>{
+//     timer.start();
+//   });
 
   const options = {
     enableTime: true,
@@ -49,7 +49,7 @@ startBtn.addEventListener('click', () =>{
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0].getTime());
+      // console.log(selectedDates[0].getTime());
 
       if (selectedDates[0].getTime() <= Date.now()){
         startBtn.disabled = true;
@@ -61,37 +61,38 @@ startBtn.addEventListener('click', () =>{
 
 flatpickr(dateTime, options);
 const startTime = options.selectedDates[0].getTime();
+console.log(startTime);
 
-const timer = new ReverseTimer({
-  onTick: updateReverseTimer});
+// const timer = new ReverseTimer({
+//   onTick: updateReverseTimer});
 
-ReverseTimer.stop();
+// ReverseTimer.stop();
 
-function updateReverseTimer ({ days, hours, minutes, seconds }) {
-  days.textContent = `${days}`;
-  hours.textContent = `${hours}`;
-  minutes.textContent = `${minutes}`;
-  seconds.textContent = `${seconds}`;
-};
+// function updateReverseTimer ({ days, hours, minutes, seconds }) {
+//   days.textContent = `${days}`;
+//   hours.textContent = `${hours}`;
+//   minutes.textContent = `${minutes}`;
+//   seconds.textContent = `${seconds}`;
+// };
 
-function addLeadingZero(value) {
-return String(value).padStart(2, '0');
-};
+// function addLeadingZero(value) {
+// return String(value).padStart(2, '0');
+// };
 
-function convertMs(ms) {
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+// function convertMs(ms) {
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+//   const days = Math.floor(ms / day);
+//   const hours = Math.floor((ms % day) / hour);
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-  return { days, hours, minutes, seconds };
-};
+//   return { days, hours, minutes, seconds };
+// };
 
-// 
+// // 
 
 
 
