011717fff4959457b4052581349a0ca22901016f
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-20 19:56:09 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,11 @@
+import Notiflix from 'notiflix';
+
+const delay = document.querySelector('input[name="delay"]')
+const step = document.querySelector('input[name="step"]')
+const amount = document.querySelector('input[name="amount"]')
+const form = document.querySelector()
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
@@ -6,3 +14,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+createPromise()
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
