daebd165496578fbf6de6a31fa2f3c47b8080c63
Maxim Kruchko <maxim.kruchko1@gmail.com>
2023-04-26 15:29:38 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -8,28 +8,31 @@ const form = document.querySelector('.form');
 const delay = Number(form.delay.value);
 const step = Number(form.step.value);
 const amount = Number(form.amount.value);
-// console.log(amount);
+
 
 form.addEventListener('submit', onSubmit);
 
 function onSubmit (evt){
   evt.preventDefault();
   for (let i = 0; i < amount; i += 1) {
-    createPromise(i, delay +1 * step)
+    createPromise(i, delay + 1 * step)
     .then(value => Notiflix.Notify.success(value))
     .catch(value => Notiflix.Notify.warning(value))
   }
 }
 
-function createPromise(position, delay) {
+// function createPromise(position, delay) {
+  function createPromise(delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
 
     setTimeout(() => {
     if (shouldResolve) {
-      resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      // resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      resolve(`✅ Fulfilled promise  ${delay}ms`);
     } else {
-      reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      // reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      reject(`❌ Rejected promise  ${delay}ms`);
     }
   }, delay);
 });
